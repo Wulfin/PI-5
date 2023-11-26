060b1f000cb58fb25b797efe1a04a591ffe00457
Wulfin <saifelislambettaoui1955@gmail.com>
2023-11-26 14:25:45 +0100

    fixing credentails as env variables

--- Tweets-scrapper/scrapper.py
+++ Tweets-scrapper/scrapper.py
@@ -1,19 +1,20 @@
 from flask import Flask, jsonify
 import tweepy
+import os
 
 app = Flask(__name__)
 
 # Authenticate to Twitter
-consumer_key = 'YOUR_CONSUMER_KEY'
-consumer_secret = 'YOUR_CONSUMER_SECRET'
-access_token = 'YOUR_ACCESS_TOKEN'
-access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
+consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
+consumer_secret = os.getenv("TWITTER_CONSUMER_SECRET")
+access_token = os.getenv("TWITTER_ACCESS_TOKEN")
+access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 
 auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # Route to get tweets from a specific user
-@app.route('/get_tweets/<username>')
+@app.route('/tweets/<username>')
 def get_tweets(username):
     try:
         # Retrieve tweets from the user's timeline
