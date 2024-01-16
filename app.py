e54e29906e9b7727c444c19ee0df955c14250b16
Otman Malki <otmanmalki57@gmail.com>
2024-01-16 19:20:39 +0100

    sentiment analysis 3rd commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, jsonify
+from flask import Flask, jsonify, json
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -170,6 +170,11 @@ def naive_model(X_train, X_test, y_train, y_test):
     conf_matrix = confusion_matrix(y_test, predictions, labels=[True, False])
     plot_confusion_matrix(conf_matrix)
 
+def save_to_json(tweet_data):
+    filename = "tweets.json"  # JSON filename
+    with open(filename, "w", encoding="utf-8") as jsonfile:
+        json.dump(tweet_data, jsonfile, indent=4)  # Write tweet data to JSON file
+
 
 @app.route('/sentiments/<search_term>')
 def sentiment_analysis(search_term):
@@ -201,126 +206,126 @@ def sentiment_analysis(search_term):
 
         # Removing tweets with empty text
         df = df[df['tidy_tweets'] != '']
-
-        # Dropping duplicate rows
-        df.drop_duplicates(subset=['tidy_tweets'], keep=False)
-
-        # Resetting index
-        df = df.reset_index(drop=True)
-
-        # Removing Punctuations, Numbers and Special characters
-        df['absolute_tidy_tweets'] = df['tidy_tweets'].str.replace("[^a-zA-Z# ]", "")
-
-        # Removing Stop words
-        stopwords_set = set(stopwords)
-        cleaned_tweets = []
-        for index, row in df.iterrows():
-            # filerting out all the stopwords
-            words_without_stopwords = [word for word in row.absolute_tidy_tweets.split() if
-                                       not word in stopwords_set and '#' not in word.lower()]
-
-            # finally creating tweets list of tuples containing stopwords(list) and sentimentType
-            cleaned_tweets.append(' '.join(words_without_stopwords))
-        df['absolute_tidy_tweets'] = cleaned_tweets
-
-        # Tokenize *'absolute_tidy_tweets'*
-        tokenized_tweet = df['absolute_tidy_tweets'].apply(lambda x: x.split())
-
-        # Converting words to Lemma
-        word_lemmatizer = WordNetLemmatizer()
-        tokenized_tweet = tokenized_tweet.apply(lambda x: [word_lemmatizer.lemmatize(i) for i in x])
-
-        # Joining all tokens into sentences
-        for i, tokens in enumerate(tokenized_tweet):
-            tokenized_tweet[i] = ' '.join(tokens)
-        df['absolute_tidy_tweets'] = tokenized_tweet
-
-        # Grammatical rule to identify phrases
-        sentence_re = r'(?:(?:[A-Z])(?:.[A-Z])+.?)|(?:\w+(?:-\w+)*)|(?:\$?\d+(?:.\d+)?%?)|(?:...|)(?:[][.,;"\'?():-_`])'
-        grammar = r"""
-            NBAR:
-                {<NN.*|JJ>*<NN.*>}  # Nouns and Adjectives, terminated with Nouns
-
-            NP:
-                {<NBAR>}
-                {<NBAR><IN><NBAR>}  # Above, connected with in/of/etc...
-        """
-        chunker = nltk.RegexpParser(grammar)
-
-        # New feature called 'key_phrases', will contain phrases for corresponding tweet
-        textblob_key_phrases = []
-        extractor = ConllExtractor()
-        for index, row in df.iterrows():
-            # filerting out all the hashtags
-            words_without_hash = [word for word in row.tidy_tweets.split() if '#' not in word.lower()]
-            hash_removed_sentence = ' '.join(words_without_hash)
-            blob = TextBlob(hash_removed_sentence, np_extractor=extractor)
-            textblob_key_phrases.append(list(blob.noun_phrases))
-        textblob_key_phrases[:10]
-        df['key_phrases'] = textblob_key_phrases
-
-        # Story Generation and Visualization
-
-        all_words = ' '.join([text for text in df['absolute_tidy_tweets'][df.sentiment == 'Positive']])
-        generate_wordcloud(all_words)
-
-        # Most common words in negative tweets
-        all_words = ' '.join([text for text in df['absolute_tidy_tweets'][df.sentiment == 'Negative']])
-        generate_wordcloud(all_words)
-
-        # Most commonly used Hashtags
-
-        hashtags = hashtag_extract(df['tidy_tweets'])
-        hashtags = sum(hashtags, [])
-
-        # For sake of consistency, we are going to discard the records which contains no phrases i.e where tweets_df['key_phrases'] contains []
-        df2 = df[df['key_phrases'].str.len() > 0]
-
-        # Feature Extraction
-
-        # Feature Extraction for 'Key Words'
-        # BOW features
-        bow_word_vectorizer = CountVectorizer(max_df=0.90, min_df=2, stop_words='english')
-        # bag-of-words feature matrix
-        bow_word_feature = bow_word_vectorizer.fit_transform(df2['absolute_tidy_tweets'])
-        # TF-IDF features
-        tfidf_word_vectorizer = TfidfVectorizer(max_df=0.90, min_df=2, stop_words='english')
-        # TF-IDF feature matrix
-        tfidf_word_feature = tfidf_word_vectorizer.fit_transform(df2['absolute_tidy_tweets'])
-
-        # Feature Extraction for 'Key Phrases'
-        phrase_sents = df2['key_phrases'].apply(lambda x: ' '.join(x))
-        # BOW phrase features
-        bow_phrase_vectorizer = CountVectorizer(max_df=0.90, min_df=2)
-        bow_phrase_feature = bow_phrase_vectorizer.fit_transform(phrase_sents)
-        # TF-IDF phrase feature
-        tfidf_phrase_vectorizer = TfidfVectorizer(max_df=0.90, min_df=2)
-        tfidf_phrase_feature = tfidf_phrase_vectorizer.fit_transform(phrase_sents)
-
-        # Model Building: Sentiment Analysis
-
-        # Map target variables to {0, 1}
-        target_variable = df2['sentiment'].apply(lambda x: 0 if x == 'neg' else 1)
-
-        # Predictions on 'key words' based features
-        # BOW word features
-        X_train, X_test, y_train, y_test = train_test_split(bow_word_feature, target_variable, test_size=0.3,
-                                                            random_state=272)
-        naive_model(X_train, X_test, y_train, y_test)
-        # TF-IDF word features
-        X_train, X_test, y_train, y_test = train_test_split(tfidf_word_feature, target_variable, test_size=0.3,
-                                                            random_state=272)
-        naive_model(X_train, X_test, y_train, y_test)
-
-        # Predictions on 'key phrases' based features
-        # BOW Phrase features
-        X_train, X_test, y_train, y_test = train_test_split(bow_phrase_feature, target_variable, test_size=0.3,
-                                                            random_state=272)
-        naive_model(X_train, X_test, y_train, y_test)
-        # TF-IDF Phrase features
-        X_train, X_test, y_train, y_test = train_test_split(tfidf_phrase_feature, target_variable, test_size=0.3,
-                                                            random_state=272)
-        naive_model(X_train, X_test, y_train, y_test)
+        #
+        # # Dropping duplicate rows
+        # df.drop_duplicates(subset=['tidy_tweets'], keep=False)
+        #
+        # # Resetting index
+        # df = df.reset_index(drop=True)
+        #
+        # # Removing Punctuations, Numbers and Special characters
+        # df['absolute_tidy_tweets'] = df['tidy_tweets'].str.replace("[^a-zA-Z# ]", "")
+        #
+        # # Removing Stop words
+        # stopwords_set = set(stopwords)
+        # cleaned_tweets = []
+        # for index, row in df.iterrows():
+        #     # filerting out all the stopwords
+        #     words_without_stopwords = [word for word in row.absolute_tidy_tweets.split() if
+        #                                not word in stopwords_set and '#' not in word.lower()]
+        #
+        #     # finally creating tweets list of tuples containing stopwords(list) and sentimentType
+        #     cleaned_tweets.append(' '.join(words_without_stopwords))
+        # df['absolute_tidy_tweets'] = cleaned_tweets
+        #
+        # # Tokenize *'absolute_tidy_tweets'*
+        # tokenized_tweet = df['absolute_tidy_tweets'].apply(lambda x: x.split())
+        #
+        # # Converting words to Lemma
+        # word_lemmatizer = WordNetLemmatizer()
+        # tokenized_tweet = tokenized_tweet.apply(lambda x: [word_lemmatizer.lemmatize(i) for i in x])
+        #
+        # # Joining all tokens into sentences
+        # for i, tokens in enumerate(tokenized_tweet):
+        #     tokenized_tweet[i] = ' '.join(tokens)
+        # df['absolute_tidy_tweets'] = tokenized_tweet
+        #
+        # # Grammatical rule to identify phrases
+        # sentence_re = r'(?:(?:[A-Z])(?:.[A-Z])+.?)|(?:\w+(?:-\w+)*)|(?:\$?\d+(?:.\d+)?%?)|(?:...|)(?:[][.,;"\'?():-_`])'
+        # grammar = r"""
+        #     NBAR:
+        #         {<NN.*|JJ>*<NN.*>}  # Nouns and Adjectives, terminated with Nouns
+        #
+        #     NP:
+        #         {<NBAR>}
+        #         {<NBAR><IN><NBAR>}  # Above, connected with in/of/etc...
+        # """
+        # chunker = nltk.RegexpParser(grammar)
+        #
+        # # New feature called 'key_phrases', will contain phrases for corresponding tweet
+        # textblob_key_phrases = []
+        # extractor = ConllExtractor()
+        # for index, row in df.iterrows():
+        #     # filerting out all the hashtags
+        #     words_without_hash = [word for word in row.tidy_tweets.split() if '#' not in word.lower()]
+        #     hash_removed_sentence = ' '.join(words_without_hash)
+        #     blob = TextBlob(hash_removed_sentence, np_extractor=extractor)
+        #     textblob_key_phrases.append(list(blob.noun_phrases))
+        # textblob_key_phrases[:10]
+        # df['key_phrases'] = textblob_key_phrases
+        #
+        # # Story Generation and Visualization
+        #
+        # all_words = ' '.join([text for text in df['absolute_tidy_tweets'][df.sentiment == 'Positive']])
+        # generate_wordcloud(all_words)
+        #
+        # # Most common words in negative tweets
+        # all_words = ' '.join([text for text in df['absolute_tidy_tweets'][df.sentiment == 'Negative']])
+        # generate_wordcloud(all_words)
+        #
+        # # Most commonly used Hashtags
+        #
+        # hashtags = hashtag_extract(df['tidy_tweets'])
+        # hashtags = sum(hashtags, [])
+        #
+        # # For sake of consistency, we are going to discard the records which contains no phrases i.e where tweets_df['key_phrases'] contains []
+        # df2 = df[df['key_phrases'].str.len() > 0]
+        #
+        # # Feature Extraction
+        #
+        # # Feature Extraction for 'Key Words'
+        # # BOW features
+        # bow_word_vectorizer = CountVectorizer(max_df=0.90, min_df=2, stop_words='english')
+        # # bag-of-words feature matrix
+        # bow_word_feature = bow_word_vectorizer.fit_transform(df2['absolute_tidy_tweets'])
+        # # TF-IDF features
+        # tfidf_word_vectorizer = TfidfVectorizer(max_df=0.90, min_df=2, stop_words='english')
+        # # TF-IDF feature matrix
+        # tfidf_word_feature = tfidf_word_vectorizer.fit_transform(df2['absolute_tidy_tweets'])
+        #
+        # # Feature Extraction for 'Key Phrases'
+        # phrase_sents = df2['key_phrases'].apply(lambda x: ' '.join(x))
+        # # BOW phrase features
+        # bow_phrase_vectorizer = CountVectorizer(max_df=0.90, min_df=2)
+        # bow_phrase_feature = bow_phrase_vectorizer.fit_transform(phrase_sents)
+        # # TF-IDF phrase feature
+        # tfidf_phrase_vectorizer = TfidfVectorizer(max_df=0.90, min_df=2)
+        # tfidf_phrase_feature = tfidf_phrase_vectorizer.fit_transform(phrase_sents)
+        #
+        # # Model Building: Sentiment Analysis
+        #
+        # # Map target variables to {0, 1}
+        # target_variable = df2['sentiment'].apply(lambda x: 0 if x == 'neg' else 1)
+        #
+        # # Predictions on 'key words' based features
+        # # BOW word features
+        # X_train, X_test, y_train, y_test = train_test_split(bow_word_feature, target_variable, test_size=0.3,
+        #                                                     random_state=272)
+        # naive_model(X_train, X_test, y_train, y_test)
+        # # TF-IDF word features
+        # X_train, X_test, y_train, y_test = train_test_split(tfidf_word_feature, target_variable, test_size=0.3,
+        #                                                     random_state=272)
+        # naive_model(X_train, X_test, y_train, y_test)
+        #
+        # # Predictions on 'key phrases' based features
+        # # BOW Phrase features
+        # X_train, X_test, y_train, y_test = train_test_split(bow_phrase_feature, target_variable, test_size=0.3,
+        #                                                     random_state=272)
+        # naive_model(X_train, X_test, y_train, y_test)
+        # # TF-IDF Phrase features
+        # X_train, X_test, y_train, y_test = train_test_split(tfidf_phrase_feature, target_variable, test_size=0.3,
+        #                                                     random_state=272)
+        # naive_model(X_train, X_test, y_train, y_test)
 
         # Extract the required information for each tweet
         tweets_result = []
@@ -336,14 +341,15 @@ def sentiment_analysis(search_term):
             }
             tweets_result.append(tweet_result)
 
-    print(df.head())
-    print(df.iterrows())
-    print(df['tidy_tweets'])
-    print(df['sentiment'])
+    # print(df.head())
+    # print(df.iterrows())
+    # print(df['tidy_tweets'])
+    # print(df['sentiment'])
     # print(tokenized_tweet)
     # generate_hashtag_freqdist(hashtags)
     # print(df2)
-    return jsonify({"tweets": tweets_result})
+    save_to_json(tweets_result)
+    return tweets_result
 
 if __name__ == '__main__':
     app.run(debug=True, port=5002)
